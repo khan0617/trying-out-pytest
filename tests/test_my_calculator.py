773199f8dcf7b43391c6ec689846719e241d218f
Hamza Khan <hamza55014@gmail.com>
2024-04-23 18:59:29 -0500

    add parametrized myCalculator.subtract test

--- tests/test_my_calculator.py
+++ tests/test_my_calculator.py
@@ -7,6 +7,16 @@ def my_calc_inst() -> MyCalculator:
     return MyCalculator
 
 
+# 4.5 Using the fixture with parametrized function
+@pytest.mark.parametrize("a, b, expected", [
+    (-1, -1, 0),
+    (0, 0, 0),
+    (5, 3, 2),
+])
+def test_my_calculator_subtract(a: float, b: float, expected: float, my_calc_inst: MyCalculator) -> None:
+    assert my_calc_inst.subtract(a, b) == expected
+
+
 # 5. showing pytest.raises for catching exceptions
 def test_my_calculator_divide_by_zero(my_calc_inst: MyCalculator) -> None:
     with pytest.raises(ZeroDivisionError) as excinfo:
