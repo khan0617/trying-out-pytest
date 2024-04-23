17bb140dbf3df47964c8fababea2c4f5b3ebb62e
Hamza Khan <hamza55014@gmail.com>
2024-04-23 18:55:48 -0500

    uncomment tests and rename calculator.py

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,5 +1,5 @@
 import pytest
-from calculator import add, multiply
+from calculator_funcs import add, multiply
 
 """
 - First we'll run the system with just 'test_add'.
@@ -12,17 +12,17 @@ def test_add() -> None:
 
 
 # 2. pytest error messages in the terminal
-# def test_multiply() -> None:
-#     assert multiply(5, 6) == 30
+def test_multiply() -> None:
+    assert multiply(5, 6) == 30
 
 
 # 3. pytest parametrization (multiple inputs with one test)
-# @pytest.mark.parametrize('a, b, expected', [
-#     (1, 1, 2),
-#     (5, 6, 11),
-#     (-100, -200, -300)
-# ])
-# def test_add_multiple_values(a: float, b: float, expected: float) -> None:
-#     assert add(a, b) == expected
+@pytest.mark.parametrize('a, b, expected', [
+    (1, 1, 2),
+    (5, 6, 11),
+    (-100, -200, -300)
+])
+def test_add_multiple_values(a: float, b: float, expected: float) -> None:
+    assert add(a, b) == expected
 
 # 7. finally showcase the pytest-cov plugin, run "pytest --cov" in the command line.
--- tests/test_my_calculator.py
+++ tests/test_my_calculator.py
@@ -2,19 +2,19 @@ import pytest
 from my_calculator import MyCalculator
 
 # 4. Pytest fixtures (like @Before in JUnit)
-# @pytest.fixture
-# def myCalcInst() -> MyCalculator:
-#     return MyCalculator
+@pytest.fixture
+def my_calc_inst() -> MyCalculator:
+    return MyCalculator
 
 
 # 5. showing pytest.raises for catching exceptions
-# def test_my_calculator_divide_by_zero(myCalcInst: MyCalculator) -> None:
-#     with pytest.raises(ZeroDivisionError) as excinfo:
-#         myCalcInst.divide(1, 0)
+def test_my_calculator_divide_by_zero(my_calc_inst: MyCalculator) -> None:
+    with pytest.raises(ZeroDivisionError) as excinfo:
+        my_calc_inst.divide(1, 0)
 
 
 # 6. Skipping a test (note the 's' in the pytest output when we skip the test!)
-# @pytest.mark.skip(reason="Complex addition not supported yet")
-# def test_complex_number_add(myCalcInst: MyCalculator) -> None:
-#     assert myCalcInst.complexAdd(2 + 3j, 4 + 5j) == 6 + 8j
+@pytest.mark.skip(reason="Complex addition not supported yet")
+def test_complex_number_add(my_calc_inst: MyCalculator) -> None:
+    assert my_calc_inst.complex_add(2 + 3j, 4 + 5j) == 6 + 8j
     
