057f756b061a4e9c236d7652ac0800254a2f1b89
Hamza Khan <hamza55014@gmail.com>
2024-04-23 19:33:32 -0500

    add self arg to MyCalculator methods

--- my_calculator.py
+++ my_calculator.py
@@ -1,16 +1,13 @@
-# step 3: use the MyCalculator class to demonstrate:
-#   - pytest fixtures (like @Before)
-#   - pytest.raises
 class MyCalculator:
-    def add(a: float, b: float) -> float:
+    def add(self, a: float, b: float) -> float:
         return a + b
     
-    def subtract(a: float, b: float) -> float:
+    def subtract(self, a: float, b: float) -> float:
         return a - b
     
-    def multiply(a: float, b: float) -> float:
+    def multiply(self, a: float, b: float) -> float:
         return a * b
     
-    def divide(a: float, b: float) -> float:
+    def divide(self, a: float, b: float) -> float:
         return a / b
     
